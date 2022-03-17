540ab110b21aef1a51ba7c1813a53c4fca76abc9
Shubh Sharma <shubhsharma2022@gmail.com>
2022-03-17 15:58:29 -0600

    moved

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { getInput, setFailed, warning } from '@actions/core';
 import { context } from '@actions/github';
 
-import {getAllData, getData} from './get-data';
+import { getAllData, getData } from './get-data';
 import makeComment from './make-comment';
 import sendDataComment from './send-data';
 import sendDataDiff from './send-data-diff';
@@ -9,17 +9,16 @@ import testMonoRepo from './test-mono-repo';
 
 const run = async (): Promise<void> => {
   try {
-    const prData = await getData();
-
-    const monoRepo = getInput('monoRepo')
+    const monoRepo = getInput('monoRepo');
     const url = getInput('coverageEndpoint');
 
-    if(monoRepo === 'true'){
-       await testMonoRepo(url, getAllData())
+    if (monoRepo === 'true') {
+      await testMonoRepo(url, getAllData());
 
-      return
+      return;
     }
 
+    const prData = await getData();
     const authToken = getInput('coverageToken');
 
     if (!authToken && url) {
