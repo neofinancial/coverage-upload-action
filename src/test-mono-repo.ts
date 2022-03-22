9a2ff7522e3a055c2eb62dc8f2d02d4b992b3946
leah <leah.copeland@neofinancial.com>
2022-03-22 08:23:36 -0600

    send header to identify request

--- src/test-mono-repo.ts
+++ src/test-mono-repo.ts
@@ -5,7 +5,10 @@ const testMonoRepo = async (url: string, allData: unknown): Promise<void> => {
   console.log('data being sent, represents entire payload', allData)
 
   try {
-    const response = await axios.post(url, allData);
+    const response = await axios.post(url, allData, {
+      headers: { repoType: 'monorepo' },
+    });
+
 
     console.log('RECIEVED RESPONSE',response.data.message)
   } catch (error) {
