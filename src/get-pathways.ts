ae59154140cd261f21a0074b8f611eb809c57019
leah <leah.copeland@neofinancial.com>
2022-03-29 08:43:42 -0600

    other pathways

--- src/get-pathways.ts
+++ src/get-pathways.ts
@@ -3,7 +3,11 @@ import globby from 'globby';
 const getPathways = async (coverageData: string): Promise<string[]> => {
 
   if (coverageData.includes(',')) {
-    const paths = await globby(coverageData.split(','));
+    const separated = coverageData.split(',')
+
+    console.log("split",separated)
+
+    const paths = await globby(separated);
 
     return paths
   }
