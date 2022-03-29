b724b0bac12aef02ce4441c7442843ce758bf369
leah <leah.copeland@neofinancial.com>
2022-03-29 07:20:56 -0600

    handle instance where coverData is one pattern

--- src/get-pathways.ts
+++ src/get-pathways.ts
@@ -1,15 +1,17 @@
 import globby from 'globby';
 
 const getPathways = async (coverageData: string): Promise<string[]> => {
+
   if (coverageData.includes(',')) {
-    const separated = coverageData.split(',');
-
-    const paths = await globby(separated);
-
-    return paths
+    coverageData.split(',');
   }
 
-  return [coverageData];
+  console.log('coverageData',coverageData)
+
+  const paths = await globby(coverageData);
+
+  return paths
+
 };
 
 export default getPathways;
