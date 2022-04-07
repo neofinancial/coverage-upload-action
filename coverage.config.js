bd20eae5c0e4308900e37eb81c1a91ed30aaea7c
leah <leah.copeland@neofinancial.com>
2022-04-07 11:41:21 -0600

    test config and lcov file

---
+++ coverage.config.js
@@ -0,0 +1,5 @@
+export const coverage = [
+  {path: '/coverage/lcov.info', displayName: 'coverage-upload-action'},
+  {path: '/packages/meow-package/coverage/lcov.info', displayName: 'meow package'}
+
+]
