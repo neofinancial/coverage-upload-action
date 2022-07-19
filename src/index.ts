d4b22f8b57a82eed3d10cb315a0ba957e9d156a1
Amber Bhatt <amber.bhatt@neofinancial.com>
2022-07-19 15:10:43 -0600

    added debug env variable and check

--- src/index.ts
+++ src/index.ts
@@ -30,38 +30,44 @@ const run = async (): Promise<void> => {
       }
     }
 
-    console.log(`Repo ID: ${prData.repositoryId}`);
-    console.log(`Ref of branch being merged: ${prData.ref}`);
-    console.log(`Ref of branch being merged into: ${prData.baseRef}`);
-    console.log(`SHA of merge commit: ${prData.sha}`);
-    console.log(`PR creator: ${prData.actor}`);
-    console.log(`Time PR created: ${prData.timestamp}`);
-    console.log(`Lines percent: ${prData.coverage.lines.percent}`);
-    console.log(`Functions percent: ${prData.coverage.functions.percent}`);
-    console.log(`Branches percent: ${prData.coverage.branches.percent}`);
+    if (process.env.ACTION_DEBUG === 'true') {
+      console.log(`Repo ID: ${prData.repositoryId}`);
+      console.log(`Ref of branch being merged: ${prData.ref}`);
+      console.log(`Ref of branch being merged into: ${prData.baseRef}`);
+      console.log(`SHA of merge commit: ${prData.sha}`);
+      console.log(`PR creator: ${prData.actor}`);
+      console.log(`Time PR created: ${prData.timestamp}`);
+      console.log(`Lines percent: ${prData.coverage.lines.percent}`);
+      console.log(`Functions percent: ${prData.coverage.functions.percent}`);
+      console.log(`Branches percent: ${prData.coverage.branches.percent}`);
 
-    if (prData.coverage.lines.diff || prData.coverage.lines.diff === 0) {
-      console.log(`Lines difference: ${prData.coverage.lines.diff}`);
-    }
+      if (prData.coverage.lines.diff || prData.coverage.lines.diff === 0) {
+        console.log(`Lines difference: ${prData.coverage.lines.diff}`);
+      }
 
-    if (prData.coverage.functions.diff || prData.coverage.functions.diff === 0) {
-      console.log(`Functions difference: ${prData.coverage.functions.diff}`);
-    }
+      if (prData.coverage.functions.diff || prData.coverage.functions.diff === 0) {
+        console.log(`Functions difference: ${prData.coverage.functions.diff}`);
+      }
 
-    if (prData.coverage.branches.diff || prData.coverage.branches.diff === 0) {
-      console.log(`Branches Difference: ${prData.coverage.branches.diff}`);
+      if (prData.coverage.branches.diff || prData.coverage.branches.diff === 0) {
+        console.log(`Branches Difference: ${prData.coverage.branches.diff}`);
+      }
+
+      if (prData.message) {
+        console.log(`Message: ${prData.message}`);
+      }
+
+      if (prData.pullRequest) {
+        console.log(`Pull Request Number: ${prData.pullRequest}`);
+      }
+
+      if (context.payload.pull_request) {
+        makePullRequestComment(prData.message, prData.coverage);
+      }
     }
 
     if (prData.message) {
-      console.log(`Message: ${prData.message}`);
-    }
-
-    if (prData.pullRequest) {
-      console.log(`Pull Request Number: ${prData.pullRequest}`);
-    }
-
-    if (context.payload.pull_request) {
-      makePullRequestComment(prData.message, prData.coverage);
+      console.log(prData.message);
     }
   } catch (error) {
     setFailed(`Coverage action failed to run: ${error.message}`);
