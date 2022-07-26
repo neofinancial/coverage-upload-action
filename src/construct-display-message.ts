e7d4cf8e49e562df1bc4ec05414b1114213423b5
Amber Bhatt <amber.bhatt@neofinancial.com>
2022-07-26 16:10:45 -0600

    more trimming

--- src/construct-display-message.ts
+++ src/construct-display-message.ts
@@ -11,36 +11,27 @@ const constructDisplayMessage = (prData: PullRequestData): displayMessageObject[
     name: 'Lines',
     coverage: `${prData.coverage.lines.percent.toFixed(2)}%`,
     differenceAfterPR: prData.message
-      ? `${prData.message.split('\n')[4].split('|')[3].replace(/ /g, '')} ${prData.message
-          .split('\n')[4]
-          .split('|')[0]
-          .trim()} `
+      ? `${prData.message.split('\n')[4].split('|')[3].trim()} ${prData.message.split('\n')[4].split('|')[0].trim()} `
       : 'No Data',
-    test: prData.message ? `${prData.message.split('\n')[4].split('|')[4].replace(/ /g, '')}` : 'No Data',
+    test: prData.message ? `${prData.message.split('\n')[4].split('|')[4].trim()}` : 'No Data',
   };
 
   const functionsCoverage = {
     name: 'Functions',
     coverage: `${prData.coverage.functions.percent.toFixed(2)}%`,
     differenceAfterPR: prData.message
-      ? `${prData.message.split('\n')[5].split('|')[3].replace(/ /g, '')} ${prData.message
-          .split('\n')[5]
-          .split('|')[0]
-          .trim()} `
+      ? `${prData.message.split('\n')[5].split('|')[3].trim()} ${prData.message.split('\n')[5].split('|')[0].trim()} `
       : 'No Data',
-    test: prData.message ? `${prData.message.split('\n')[5].split('|')[4].replace(/ /g, '')}` : 'No Data',
+    test: prData.message ? `${prData.message.split('\n')[5].split('|')[4].trim()}` : 'No Data',
   };
 
   const branchesCoverage = {
     name: 'Branches',
     coverage: `${prData.coverage.branches.percent.toFixed(2)}%`,
     differenceAfterPR: prData.message
-      ? `${prData.message.split('\n')[6].split('|')[3].replace(/ /g, '')} ${prData.message
-          .split('\n')[6]
-          .split('|')[0]
-          .trim()} `
+      ? `${prData.message.split('\n')[6].split('|')[3].trim()} ${prData.message.split('\n')[6].split('|')[0].trim()} `
       : 'No Data',
-    test: prData.message ? `${prData.message.split('\n')[6].split('|')[4].replace(/ /g, '')}` : 'No Data',
+    test: prData.message ? `${prData.message.split('\n')[6].split('|')[4].trim()}` : 'No Data',
   };
 
   return [linesCoverage, functionsCoverage, branchesCoverage];
