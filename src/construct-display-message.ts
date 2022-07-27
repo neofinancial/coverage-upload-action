cd513eb07399d388a9699571dc6c7e60b035786d
Amber Bhatt <amber.bhatt@neofinancial.com>
2022-07-27 14:50:29 -0600

    removed last column

--- src/construct-display-message.ts
+++ src/construct-display-message.ts
@@ -10,22 +10,25 @@ const constructDisplayMessage = (prData: PullRequestData): displayMessageObject[
   const linesCoverage = {
     name: 'Lines',
     coverage: `${prData.coverage.lines.percent.toFixed(2)}%`,
-    differenceAfterPR: prData.message ? `${prData.message.split('\n')[4].split('|')[3]}`.trim() : 'No Data',
-    ___________________: prData.message ? `${prData.message.split('\n')[4].split('|')[4].trim()}` : 'No Data',
+    differenceAfterPR: prData.message
+      ? `${prData.message.split('\n')[4].split('|')[3].trim()}${prData.message.split('\n')[4].split('|')[4].trim()}`
+      : 'No Data',
   };
 
   const functionsCoverage = {
     name: 'Functions',
     coverage: `${prData.coverage.functions.percent.toFixed(2)}%`,
-    differenceAfterPR: prData.message ? `${prData.message.split('\n')[5].split('|')[3]}`.trim() : 'No Data',
-    ___________________: prData.message ? `${prData.message.split('\n')[5].split('|')[4].trim()}` : 'No Data',
+    differenceAfterPR: prData.message
+      ? `${prData.message.split('\n')[5].split('|')[3].trim()}${prData.message.split('\n')[5].split('|')[4].trim()}`
+      : 'No Data',
   };
 
   const branchesCoverage = {
     name: 'Branches',
     coverage: `${prData.coverage.branches.percent.toFixed(2)}%`,
-    differenceAfterPR: prData.message ? `${prData.message.split('\n')[6].split('|')[3]}`.trim() : 'No Data',
-    ___________________: prData.message ? `${prData.message.split('\n')[6].split('|')[4].trim()}` : 'No Data',
+    differenceAfterPR: prData.message
+      ? `${prData.message.split('\n')[6].split('|')[3].trim()}${prData.message.split('\n')[6].split('|')[4].trim()}`
+      : 'No Data',
   };
 
   return [linesCoverage, functionsCoverage, branchesCoverage];
