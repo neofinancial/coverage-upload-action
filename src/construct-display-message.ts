594f4dd7fda8083ee846c5027e49ee1a219a54d9
Amber Bhatt <amber.bhatt@neofinancial.com>
2022-07-27 14:53:15 -0600

    re-adding emote column

--- src/construct-display-message.ts
+++ src/construct-display-message.ts
@@ -3,32 +3,29 @@ import { PullRequestData } from './types';
 interface displayMessageObject {
   name: string;
   coverage: string;
-  differenceAfterPR: string;
+  difference: string;
 }
 
 const constructDisplayMessage = (prData: PullRequestData): displayMessageObject[] => {
   const linesCoverage = {
     name: 'Lines',
     coverage: `${prData.coverage.lines.percent.toFixed(2)}%`,
-    differenceAfterPR: prData.message
-      ? `${prData.message.split('\n')[4].split('|')[3].trim()}${prData.message.split('\n')[4].split('|')[4].trim()}`
-      : 'No Data',
+    difference: prData.message ? `${prData.message.split('\n')[4].split('|')[3]}`.trim() : 'No Data',
+    '': prData.message ? `${prData.message.split('\n')[4].split('|')[4].trim()}` : 'No Data',
   };
 
   const functionsCoverage = {
     name: 'Functions',
     coverage: `${prData.coverage.functions.percent.toFixed(2)}%`,
-    differenceAfterPR: prData.message
-      ? `${prData.message.split('\n')[5].split('|')[3].trim()}${prData.message.split('\n')[5].split('|')[4].trim()}`
-      : 'No Data',
+    difference: prData.message ? `${prData.message.split('\n')[5].split('|')[3]}`.trim() : 'No Data',
+    '': prData.message ? `${prData.message.split('\n')[5].split('|')[4].trim()}` : 'No Data',
   };
 
   const branchesCoverage = {
     name: 'Branches',
     coverage: `${prData.coverage.branches.percent.toFixed(2)}%`,
-    differenceAfterPR: prData.message
-      ? `${prData.message.split('\n')[6].split('|')[3].trim()}${prData.message.split('\n')[6].split('|')[4].trim()}`
-      : 'No Data',
+    difference: prData.message ? `${prData.message.split('\n')[6].split('|')[3]}`.trim() : 'No Data',
+    '': prData.message ? `${prData.message.split('\n')[6].split('|')[4].trim()}` : 'No Data',
   };
 
   return [linesCoverage, functionsCoverage, branchesCoverage];
